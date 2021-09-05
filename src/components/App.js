5277af2307e277a559a7fb5ad62d83c071630154
Paul Ladd <vlendle@gmail.com>
2021-09-05 14:37:28 -0500

    Remove some create-react-app cruft and move components/styles into folders

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import '../styles/App.css';
+import { useState, useEffect } from 'react';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+export default function App() {
+
+  useEffect(() => {
+    
+  })
+
+  return (
+    <div className="App">
+      <p>hi</p>
+    </div>
+  );
+}
