f2e3136737ac6d6cd225d29f7575b231d7ca6146
Paul Ladd <vlendle@gmail.com>
2021-09-08 07:51:27 -0500

    Update gitignore to ignore build folder, update index.js to modify axios to use credentials

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import axios from 'axios';
+
+if (process.env.REACT_APP_API_URL) {
+  console.log('found a variable:', process.env.REACT_APP_API_URL)
+  axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+  axios.defaults.withCredentials = true;
+}
 
 ReactDOM.render(
   <React.StrictMode>
