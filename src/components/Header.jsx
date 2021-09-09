02ab9adbd513718d365ed543522cef32482c6311
Paul Ladd <vlendle@gmail.com>
2021-09-09 07:50:10 -0500

    Update app name

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header(props) {
   return(
     <div className="navbar navbar-expand-sm navbar-light bg-info">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">Timekeeper</Link>
+        <Link className="navbar-brand" to="/">Capensis</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
