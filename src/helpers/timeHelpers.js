f410f50a05160be94254b37ad4e492a733162a90
Paul Ladd <vlendle@gmail.com>
2021-10-11 10:36:52 -0500

    Update timeHelpers.getBoundaryMinutes to accept different argument names

--- src/helpers/timeHelpers.js
+++ src/helpers/timeHelpers.js
@@ -22,13 +22,18 @@ export function minutesSinceMidnight (hours, minutes) {
   return (hours * 60) + minutes;
 }
 
-export function getBoundaryMinutes(values) {
+// Internal helper function to allow getBoundaryMinutes to handle different options
+function getFormValuesBoundaryMinutes(values) {
   const startHours = Number(values.startHour) + Number(values.startAMPM);
   const startMinutes = Number(values.startMinute)
   const endHours = Number(values.endHour) + Number(values.endAMPM);
   const endMinutes = Number(values.endMinute)
   const startMins = minutesSinceMidnight(startHours, startMinutes);
   const endMins = minutesSinceMidnight(endHours, endMinutes);
-  console.log(`getBoundaryMinutes found: ${startMins}, ${endMins}`);
+  console.log(`getFormValuesBoundaryMinutes found: ${startMins}, ${endMins}`);
   return [startMins, endMins]
+}
+
+export function getBoundaryMinutes({values}) {
+  if (values) return getFormValuesBoundaryMinutes(values);
 }
