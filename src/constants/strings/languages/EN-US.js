d0e249c90883bb089f379ad335389cdd43e9ef85
Paul Ladd <vlendle@gmail.com>
2021-10-11 10:15:13 -0500

    Update EN-US string keys to match error keys, add noProject and endBeforeStart strings

--- src/constants/strings/languages/EN-US.js
+++ src/constants/strings/languages/EN-US.js
@@ -1,6 +1,8 @@
 const EN_US = {
   NEW_BLOCK_VALIDATION: {
-    BLOCK_CONFLICT: "This time conflicts with another block!"
+    conflict: "This time conflicts with another block: ",
+    noProject: "You must select a project.",
+    endBeforeStart: "That block would end before its start time."
   }
 }
 
