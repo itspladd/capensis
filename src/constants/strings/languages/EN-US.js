362b6b0c95032330a8def05b66210d988f9b16ed
Paul Ladd <vlendle@gmail.com>
2021-10-11 11:29:23 -0500

    Update language strings to be functions

--- src/constants/strings/languages/EN-US.js
+++ src/constants/strings/languages/EN-US.js
@@ -1,8 +1,13 @@
 const EN_US = {
   NEW_BLOCK_VALIDATION: {
-    conflict: "This time conflicts with another block: ",
-    noProject: "You must select a project.",
-    endBeforeStart: "That block would end before its start time."
+    conflict: (block) => {
+      const {title, start_time, end_time} = block;
+      const startStr = new Date(start_time).toLocaleTimeString();
+      const endStr = new Date(end_time).toLocaleTimeString();
+      return `This time conflicts with another block: ${title}, ${startStr} - ${endStr}`
+    },
+    noProject: () => "You must select a project.",
+    endBeforeStart: () => "That block would end on or before its start time."
   }
 }
 
