ae8a2adf95eea14373f0a6a50b893f0ec7246dbd
Paul Ladd <vlendle@gmail.com>
2021-10-12 14:26:25 -0500

    Update StatusBar to render with no errors when props are not present

--- src/components/StatusBar.jsx
+++ src/components/StatusBar.jsx
@@ -2,17 +2,27 @@ import '../styles/StatusBar.css';
 
 export default function StatusBar(props) {
   const { currentSession, projects} = props;
-  const tracking = currentSession.id !== undefined;
-  const projectsLoaded = projects[currentSession.project_id] !== undefined;
-  const projectTitle = tracking && projectsLoaded && projects[currentSession.project_id].title
+
+  const loading = !projects;
+  const tracking = currentSession && projects && projects[currentSession.project_id]
+
+  const bgColor = () => {
+    if (tracking) return 'bg-success text-light';
+    if (!tracking) return 'bg-light';
+  }
 
   return (
-    <div className={`statusBar mb-2 border-top border-info
-      ${tracking ? 'bg-warning' : 'bg-info'}`}>
-      { currentSession.id &&
-        `Currently tracking ${projectTitle}`
+    <div className={`statusBar mb-2 border-top border-info ${bgColor()}`}>
+      { loading &&
+        `Loading...`
       }
-      { !currentSession.id &&
+      { tracking &&
+        `Currently tracking ${projects[currentSession.project_id].title}`
+      }
+      { currentSession && !projects[currentSession.project_id] &&
+        `ERROR: Trying to track project ID ${currentSession.project_id}, but it was not found.`
+      }
+      { !currentSession &&
         `Not currently tracking`
       }
     </div>
