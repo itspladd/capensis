813c1d6686de72816846524f66841c56c79f11d9
Paul Ladd <vlendle@gmail.com>
2021-10-15 10:22:38 -0500

    Add smoke tests for Login and Register components, remove LoginRegister test

--- src/components/__tests__/Login.js
+++ src/components/__tests__/Login.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import LoginRegister from '../LoginRegister';
+import Login from '../Login';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<LoginRegister />, div)
+  ReactDOM.render(<Login />, div)
 })
