09033b075fded0907c67f555ed215da12c30730a
Paul Ladd <vlendle@gmail.com>
2021-10-18 13:04:34 -0500

    Updated Register form to use React-Bootstrap form, and fix bugs in Login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -9,54 +9,30 @@ export default function Login(props) {
   const { setUsername } = props;
 
   const [formValues, handleFormChange] = useControlledForms({
-    username: "",
-    rawPassword: ""
+    formLoginUsername: "",
+    formLoginPassword: ""
   });
 
   const handleSubmit = event => {
     event.preventDefault();
-    const { username, rawPassword } = formValues;
-    axios.post(`/api/login`, {username, rawPassword})
+    const { formLoginUsername, formLoginPassword } = formValues;
+    axios.post(`/api/login`, {username: formLoginUsername, rawPassword:formLoginPassword})
          .then(res => setUsername(res.data.username))
   }
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <Form.Group className="mb-3" controlId="username">
+    <Form className="loginForm" >
+      <Form.Group className="mb-3" controlId="formLoginUsername">
         <Form.Label>Username</Form.Label>
         <Form.Control onChange={handleFormChange} type="text" placeholder="Username" />
       </Form.Group>
-      <Form.Group className="mb-3" controlId="rawPassword">
+      <Form.Group className="mb-3" controlId="formLoginPassword">
         <Form.Label>Password</Form.Label>
         <Form.Control onChange={handleFormChange} type="password" placeholder="Password" />
       </Form.Group>
-      <Button variant="success" type="submit" >
+      <Button variant="success" type="submit" onClick={handleSubmit}>
         Submit
       </Button>
     </Form>
   )
-
-  /* Old implementation with basic forms
-  * return (
-      <div className="login m-2">
-        <form className="row row-cols-sm-auto g-3 align-items-center"
-          onSubmit={handleSubmit}>
-          <div className="col-12">
-            <label className="form-label" htmlFor="username">Username: </label>
-          </div>
-          <div className="col-12">
-            <input className="form-control" name="username" type="text" value={formValues.username} onChange={handleFormChange}></input>
-          </div>
-          <div className="col-12">
-            <label className="form-label" htmlFor="rawPassword">Password: </label>
-          </div>
-          <div className="col-12">
-            <input className="form-control" name="rawPassword" type="password" value={formValues.rawPassword} onChange={handleFormChange}></input>
-          </div>
-          <div className="col-12">
-            <button className="btn btn-success">Login</button>
-          </div>
-        </form>
-      </div>
-  ) */
 }
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,43 +1,37 @@
 import axios from 'axios';
 import useControlledForms from '../hooks/useControlledForms';
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 export default function Register(props) {
 
   const { setUsername } = props;
 
   const [formValues, handleFormChange] = useControlledForms({
-    username: "",
-    rawPassword: ""
+    formRegisterUsername: "",
+    formRegisterPassword: ""
   });
 
   const handleSubmit = event => {
     event.preventDefault();
-    const { username, rawPassword } = formValues;
-    axios.post(`/api/users`, {username, rawPassword})
+    const { formRegisterUsername, formRegisterPassword } = formValues;
+    axios.post(`/api/users`, {username: formRegisterUsername, rawPassword: formRegisterPassword})
          .then(res => setUsername(res.data.username))
   }
 
   return (
-    <div className="register m-2">
-      <form className="row row-cols-sm-auto g-3 align-items-center"
-        onSubmit={handleSubmit}>
-        <div className="col-12">
-          <label className="form-label" htmlFor="username">Username: </label>
-        </div>
-        <div className="col-12">
-          <input
-            className="form-control" name="username" type="text" value={formValues.username} onChange={handleFormChange}></input>
-        </div>
-        <div className="col-12">
-          <label className="form-label" htmlFor="rawPassword">Password: </label>
-        </div>
-        <div className="col-12">
-          <input className="form-control" name="rawPassword" type="password" value={formValues.rawPassword} onChange={handleFormChange}></input>
-        </div>
-        <div className="col-12">
-          <button className="btn btn-success">Register</button>
-        </div>
-      </form>
-    </div>
+    <Form className="registerForm" onSubmit={handleSubmit}>
+      <Form.Group className="mb-3" controlId="formRegisterUsername">
+        <Form.Label>Username</Form.Label>
+        <Form.Control onChange={handleFormChange} type="text" placeholder="Username" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formRegisterPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control onChange={handleFormChange} type="password" placeholder="Password" />
+      </Form.Group>
+      <Button variant="success" type="submit" >
+        Register
+      </Button>
+    </Form>
   )
 }
