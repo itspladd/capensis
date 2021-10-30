6bff8328e126b62c97daaa575512dc3100ddcb5e
Paul Ladd <vlendle@gmail.com>
2021-10-29 20:32:31 -0500

    Add number fudging for percentage complete so CSS bars look better

--- src/components/ReportBar.jsx
+++ src/components/ReportBar.jsx
@@ -4,10 +4,19 @@ import '../styles/ReportBar.css';
 export default function ReportBar(props) {
   const {color, progress, goal} = props
 
-  const getPercentage = (num, denom) => {
+  const percent = (num, denom) => {
     const percent = (num/denom) * 100;
-    return Math.round(percent*1000) / 1000;
+    return Math.round(percent*10) / 10;
   }
+
+  // Fudge the numbers a bit for CSS width-niceness.
+  const fudgePercentage = percent => {
+    if (percent < 1) return 1;
+    if (percent === 100) return 101;
+    return percent
+  }
+
+  const innerWidth = fudgePercentage(percent(progress, goal));
 
   const outerBarStyle = {
     backgroundColor: `${color}66`
@@ -15,14 +24,13 @@ export default function ReportBar(props) {
 
   const innerBarStyle = {
     backgroundColor: color,
-    width: `${getPercentage(progress, goal) || 1}%`
+    width: `${innerWidth}%`
   }
 
   return (
     <div className="reportBar" style={outerBarStyle}>
       <div className="reportBar-inner"
         style={innerBarStyle}>
-
         </div>
     </div>
   )
--- src/stories/ReportBar.stories.js
+++ src/stories/ReportBar.stories.js
@@ -9,7 +9,8 @@ export default {
 const Template = args => <ReportBar {...args} />;
 
 export const Unfilled = Template.bind({});
-export const Half = Template.bind({})
+export const Half = Template.bind({});
+export const Full = Template.bind({});
 
 Unfilled.args = {
   progress: 0,
@@ -21,4 +22,10 @@ Half.args = {
   progress: 50,
   goal: 100,
   color: "#00ff00"
+}
+
+Full.args = {
+  progress: 100,
+  goal: 100,
+  color: "#0000ff"
 }
