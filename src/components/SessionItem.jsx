00360ff830df9994f85810f06eda3ffecc8f9a59
Paul Ladd <vlendle@gmail.com>
2021-11-03 20:06:01 -0500

    Update session items and session list

--- src/components/SessionItem.jsx
+++ src/components/SessionItem.jsx
@@ -1,21 +1,32 @@
-import { makeTimeString } from '../helpers/stringHelpers'
+import { makeTimeString, makeWeekDayString, makeDateString } from '../helpers/stringHelpers'
 import { getTimeIntervalUnits } from '../helpers/timeHelpers'
 
-export default function SessionItem(props) {
 
+import '../styles/SessionItem.css'
+
+import ListGroupItem from 'react-bootstrap/ListGroupItem'
+
+import useControlledForms from '../hooks/useControlledForms'
+
+const LANG = 'EN-US'
+
+export default function SessionItem(props) {
   const { id, project_id, title, start_time, end_time } = props;
 
   const [h, m, s] = getTimeIntervalUnits(start_time, end_time)
 
-  const hours = h ? `${h}h` : '';
-  const mins = `${m}m`
+  const hours = h ? `${h}h ` : '';
+  const mins = `${m}m `
   const secs = `${s}s`
 
   return(
-    <div className="sessionItem">
-      <span>{title}</span>
-      <span>{makeTimeString(start_time)} to {makeTimeString(end_time)}</span>
-      <span>{hours} {mins} {secs}</span>
-    </div>
+    <ListGroupItem className="session-item" action>
+        <strong className="session-item-title">{title}</strong>
+        <span className="session-item-date">{makeWeekDayString(LANG, start_time)}, {makeDateString(LANG, start_time)}</span>
+        <p className="session-item-time">
+          <span>{makeTimeString(start_time)} to {makeTimeString(end_time)}</span>
+          <span className="text-muted"> ({hours}{mins}{secs})</span>
+        </p>
+    </ListGroupItem>
   )
 }
--- src/components/SessionList.jsx
+++ src/components/SessionList.jsx
@@ -1,8 +1,37 @@
+import { useState, useRef } from 'react'
+
+import '../styles/SessionList.css'
+
+import ListGroup from 'react-bootstrap/ListGroup'
+import Overlay from 'react-bootstrap/Overlay'
+import Tooltip from 'react-bootstrap/Tooltip'
+
+import SessionItem from './SessionItem'
+
 export default function SessionList(props) {
   const { sessions } = props;
-  return (
-    <div>
 
-    </div>
+  const target = useRef(null)
+
+  const sessionList = sessions.map(session => {
+    return (
+        <SessionItem
+          {...session}
+          ref = {target}
+          onHover={() => console.log('beans')}
+          key = {session.id}
+        />
+    )
+  })
+
+  return (
+    <ListGroup className='session-list'>
+      <ListGroup.Item className="session-item header">
+        <strong className="session-item-title">Project</strong>
+        <strong className="session-item-date">Date</strong>
+        <strong className="session-item-time">Time (click to edit)</strong>
+      </ListGroup.Item>
+      {sessionList}
+    </ListGroup>
   )
 }
