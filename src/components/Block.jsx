401465ebaddce732fcb825b707ba42101322b12d
Paul Ladd <vlendle@gmail.com>
2021-11-10 22:05:48 -0600

    Margins, background, and hour-separation lines for spacers and blocks

--- src/components/Block.jsx
+++ src/components/Block.jsx
@@ -12,27 +12,29 @@ export default function Block(props) {
   const spacer = project_id === -1;
 
   const lengthStr = spacer ? "1rem" : `${length}rem`
-  const projectFontSize = length > 1 ? "1rem" : ".6rem"
-  const intervalFontSize = length > 1 ? ".8rem" : ".6rem"
 
   const blockClass = classNames("block", "list-group-item", {
     spacer,
-    "hour-line": spacer && !getHM(end_time)[1], // if we're a spacer and end-time minutes are 0
+    project: !spacer,
     short: length === 1,
+    "hour-end": spacer &&
+                getHM(end_time)[1] === 0, // if we're a spacer and end-time minutes are 0
+    "hour-start": spacer &&
+                getHM(start_time)[1] === 0,
   });
 
   return(
     <li
       id={id}
       className={blockClass}
-      style={{height: lengthStr, fontSize: projectFontSize}}
+      style={{height: lengthStr}}
       projectid={project_id}
     >
         {!spacer &&
-        <>
+        <div className={"block-body"}>
           <p>{title}</p>
-          <span className="text-muted" style={{ fontSize: intervalFontSize}}>{interval}</span>
-        </>
+          <span className="text-muted">{interval}</span>
+        </div>
         }
     </li>
   )
