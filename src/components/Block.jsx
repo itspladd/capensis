42a2e0cf07dc9e5cebb8ba33157f43b8932318ce
Paul Ladd <vlendle@gmail.com>
2021-11-10 21:35:00 -0600

    Rework styling for block spacers to have a line on every hour and no line at the bottom

--- src/components/Block.jsx
+++ src/components/Block.jsx
@@ -1,28 +1,39 @@
 import '../styles/Block.css'
-import { makeTimeString, makeShortIntervalString } from '../helpers/stringHelpers'
+import classNames from 'classnames'
+import { makeShortIntervalString } from '../helpers/stringHelpers'
+import { getHM } from '../helpers/timeHelpers'
 
 export default function Block(props) {
-  const { title, project_id, length, start_time, end_time } = props;
+  const { id, title, project_id, length, start_time, end_time } = props;
 
   const interval = makeShortIntervalString(start_time, end_time);
 
   // Is this block a placeholder?
-  const placeholder = project_id === -1;
+  const spacer = project_id === -1;
 
-  const blockClass = placeholder ? "block_placeholder" : "block"
-  const backgroundClass = placeholder ? "block_placeholder_background" : "block_background"
-  const lengthStr = placeholder ? "1rem" : `${length}rem`
+  const lengthStr = spacer ? "1rem" : `${length}rem`
+  const projectFontSize = length > 1 ? "1rem" : ".6rem"
+  const intervalFontSize = length > 1 ? ".8rem" : ".6rem"
+
+  const blockClass = classNames("block", "list-group-item", {
+    spacer,
+    "hour-line": spacer && !getHM(end_time)[1], // if we're a spacer and end-time minutes are 0
+    short: length === 1,
+  });
+
   return(
-    <li className={"list-group-item " + backgroundClass}
-      style={{height: lengthStr}}
+    <li
+      id={id}
+      className={blockClass}
+      style={{height: lengthStr, fontSize: projectFontSize}}
+      projectid={project_id}
     >
-      <div
-        className={blockClass}
-        projectid={project_id}
-      >
-        {!placeholder && <h5>{title}</h5>}
-        {interval}
-      </div>
+        {!spacer &&
+        <>
+          <p>{title}</p>
+          <span className="text-muted" style={{ fontSize: intervalFontSize}}>{interval}</span>
+        </>
+        }
     </li>
   )
 }
