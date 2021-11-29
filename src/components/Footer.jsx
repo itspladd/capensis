42c6421158c7a9abf519356849c4ddf12c74c4a6
Paul Ladd <vlendle@gmail.com>
2021-11-29 10:06:08 -0600

    Add reset css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import '../styles/Footer.css'
 
 export default function Footer() {
   return(
-    <div className="Footer">
+    <div id="footer">
       Created by Paul Ladd. Check out the project on <a href="https://github.com/itspladd/capensis">GitHub</a>!
     </div>
   )
