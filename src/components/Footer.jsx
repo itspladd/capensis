6d9bd48e59f49058fbbc293096995dec363ed84d
Paul Ladd <vlendle@gmail.com>
2021-11-29 14:19:17 -0600

    Update footer styles and responsiveness

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,15 @@
 import '../styles/Footer.css'
+import github_logo_light from '../assets/github_logo_light.png'
+
+const repoLink = "https://github.com/itspladd/capensis"
 
 export default function Footer() {
   return(
     <div id="footer">
-      Created by <a href="https://pladd.dev">Paul Ladd</a>. Check out the project on <a href="https://github.com/itspladd/capensis">GitHub</a>!
+      <p>Created by <a href="https://pladd.dev">Paul Ladd</a></p>
+      <div>
+        <p>Hosted on <a href={repoLink}>Github</a></p>
+        <img src={github_logo_light} alt="GitHub logo"/></div>
     </div>
   )
 }
