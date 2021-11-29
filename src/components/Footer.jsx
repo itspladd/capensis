830e512bc722fc1bf1825c18864a5afbfb53a59f
Paul Ladd <vlendle@gmail.com>
2021-11-29 10:36:22 -0600

    add link to my homepage from footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import '../styles/Footer.css'
 export default function Footer() {
   return(
     <div id="footer">
-      Created by Paul Ladd. Check out the project on <a href="https://github.com/itspladd/capensis">GitHub</a>!
+      Created by <a href="https://pladd.dev">Paul Ladd</a>. Check out the project on <a href="https://github.com/itspladd/capensis">GitHub</a>!
     </div>
   )
 }
