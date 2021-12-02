9598417d21cd722ba825c38caaabd6c099b4750c
Paul Ladd <vlendle@gmail.com>
2021-12-02 11:08:22 -0600

    Change form to use fieldset element with legend

--- src/components/BlockFormModal.jsx
+++ src/components/BlockFormModal.jsx
@@ -43,50 +43,59 @@ export default function BlockFormModal(props) {
       </Modal.Header>
       <Modal.Body>
         <form className="block_form" onSubmit={submit}>
-          <span>Date: <strong>{currentDateText}</strong></span>
-          <div>
-            <label htmlFor="project">Project:</label>
+          <fieldset>
+            <span>Date: <strong>{currentDateText}</strong></span>
+          </fieldset>
+          <fieldset>
+            <legend>Project</legend>
+            <label className="visually-hidden" htmlFor="project">Project:</label>
             <select className="form-select" name="project" id="project" value={values.project} onChange={change}>
               <option value="">Select a project</option>
               {projectOptions}
             </select>
-          </div>
-          <div>
-            <label htmlFor="startHour">Starting at:</label>
+          </fieldset>
+          <fieldset>
+            <legend>Starting at:</legend>
             <ul>
+              <label className="visually-hidden" htmlFor="startHour">Start hour</label>
               <select className="form-select" name="startHour" id="startHour" value={values.startHour} onChange={change}>
                 {hoursOptions}
               </select>
+              <label className="visually-hidden" htmlFor="startMinute">Start minute</label>
               <select className="form-select" name="startMinute" id="startMinute" value={values.startMinute} onChange={change}>
                 <option>00</option>
                 <option>15</option>
                 <option>30</option>
                 <option>45</option>
               </select>
+              <label className="visually-hidden" htmlFor="startAMPM">Start AM/PM</label>
               <select className="form-select" name="startAMPM" id="startAMPM" value={values.startAMPM} onChange={change}>
                 <option value="0">AM</option>
                 <option value="12">PM</option>
               </select>
             </ul>
-          </div>
-          <div>
-            <label>Ending at:</label>
+          </fieldset>
+          <fieldset>
+            <legend>Ending at:</legend>
             <ul>
+              <label className="visually-hidden" htmlFor="endHour">End hour</label>
               <select className="form-select" name="endHour" id="endHour" value={values.endHour} onChange={change}>
                 {hoursOptions}
               </select>
+              <label className="visually-hidden" htmlFor="endMinute">End minute</label>
               <select className="form-select" name="endMinute" id="endMinute" value={values.endMinute} onChange={change}>
                 <option>00</option>
                 <option>15</option>
                 <option>30</option>
                 <option>45</option>
               </select>
+              <label className="visually-hidden" htmlFor="endAMPM">End AM/PM</label>
               <select className="form-select" name="endAMPM" id="endAMPM" value={values.endAMPM} onChange={change}>
                 <option value="0">AM</option>
                 <option value="12">PM</option>
               </select>
             </ul>
-          </div>
+          </fieldset>
           { showErrors &&
               <section>
                 {valid ?
