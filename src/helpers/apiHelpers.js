80f8e3ce56c022db207daadb075df4ca7a6a2b83
Paul Ladd <vlendle@gmail.com>
2021-12-23 11:53:34 -0600

    Add report functions to api module

--- src/helpers/apiHelpers.js
+++ src/helpers/apiHelpers.js
@@ -3,13 +3,15 @@ import * as users from './api/users'
 import * as blocks from './api/blocks'
 import * as sessions from './api/sessions'
 import * as projects from './api/projects'
+import * as reports from './api/projects'
 
 const api = {
   auth,
   users,
   blocks,
   sessions,
-  projects
+  projects,
+  reports
 }
 
 export default api;
